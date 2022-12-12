d66f2305fa187fab6b6b6afdb38a937c107b6be6
noraalobaidi <nalobaidi_@hotmail.com>
2022-12-12 17:43:55 +0300

    CREATE class classes

---
+++ src/main/java/com/nora/project/Classes.java
@@ -0,0 +1,27 @@
+package com.nora.project;
+
+public class Classes {
+
+	private int id;
+	private String roomName;
+	
+	
+	public Classes(int id, String roomName) {
+		this.id = id;
+		this.roomName = roomName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRoomName() {
+		return roomName;
+	}
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+	
+	
+}
